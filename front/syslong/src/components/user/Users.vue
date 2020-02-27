2596df9306f78e30a6ae14099376ba5e1bb35322
liufulong <qq65long@126.com>
2020-02-27 17:43:41 +0800

    添加用户管理组件和页面

--- front/syslong/src/components/user/Users.vue
+++ front/syslong/src/components/user/Users.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        <h3>用户列表组件</h3>
+      <!--面包屑导航区与-->
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item :to="{ path: '/users' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
+          <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+        </el-breadcrumb>
+      <!--卡片试图-->
+      <el-card>
+        <!--搜索与添加区域-->
+        <el-row :gutter="20">
+          <el-col :span="7">
+              <el-input placeholder="请输入内容">
+                <el-button slot="append" icon="el-icon-search"></el-button>
+              </el-input>
+          </el-col>
+          <el-col :span="3">
+              <el-button type="primary">新增用户</el-button>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
 </template>
 
