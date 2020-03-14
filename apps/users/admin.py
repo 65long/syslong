954725a45a9906e413298a369a058c6b7b7746fe
liufulong <qq65long@126.com>
2020-03-14 14:08:54 +0800

    新建分支预备添加数据权限功能

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import UserProfile, Role, WebRes
+from .models import Organization, Department
 # Register your models here.
 
-admin.site.register([UserProfile, Role, WebRes])
+admin.site.register([UserProfile, Role, WebRes, Organization, Department])
